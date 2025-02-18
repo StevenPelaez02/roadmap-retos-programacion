33cb51c4e0db9f9c26d7ce63b908a97001387094
Steven-Rengifo <steven.rengifo01@uceva.edu.co>
2025-02-18 07:20:09 -0500

    01 Steven Rengifo

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/Steven-Rengifo.js
@@ -0,0 +1,31 @@
+let carlos = "steven"
+
+let num1  = 5
+let num2 = 4
+let num3 = 2
+let num4 = 0
+
+if("steven" == carlos){
+    console.log("Iguales")
+}
+else if("steven" === steven){
+    console.log("tambien son iguales")
+} else {
+    console.log("pues no son iguales")
+}
+
+while(num4 < 5){
+    num4 = 5
+}
+
+num4 = 0
+do{
+    console.log("es menor")
+    num4++
+}while(num4 < 5)
+
+for(let i = 0; i< 15; i++){
+    if((num1 >= 6 || num2 <= 5) && num3 != 1){
+        console.log("Verdadero")
+    }
+}
