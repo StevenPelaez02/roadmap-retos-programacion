ad753a1c745b9537ded0538d24919abc7664d455
Steven-Rengifo <steven.rengifo01@uceva.edu.co>
2025-02-18 07:03:05 -0500

    ejercicio 00 Steven

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Steven-Rengifo.js
@@ -0,0 +1,19 @@
+// Documentación de JavaScript --> https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// una linea
+
+// individual, pero
+// sirve para texto
+// completo
+
+/*este es mejor para el texto*/
+
+const variable = 0
+
+int = 0
+float = 0.5
+string = "jajaaj"
+char = 'a'
+bool = true //false
+null = null
+console.log("Hola, [javaScript]")
